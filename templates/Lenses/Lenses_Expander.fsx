2609e956d1c556f1e6d6c9736cc740b5c7da47a4
Anthony Perez <anthony@amazingant.com>
2016-09-12 14:26:11 +0000

    Fix use of the Lenses helper functions
    Some of the helper names got capitalized during the rewrite from my
    personal project; forgot to capitalize them where they are used.

--- templates/Lenses/Lenses_Expander.fsx
+++ templates/Lenses/Lenses_Expander.fsx
@@ -31,7 +31,7 @@ module LensExpansion =
             let s =
                 sprintf "
             static member %s_StaticLens : StaticLens<%s, %s> =
-                makeStaticLens
+                MakeStaticLens
                     (fun x -> x.%s)
                     (fun v x -> { x with %s = v })"
                     p.Name t.Name (getTypeName p.PropertyType) p.Name p.Name
@@ -55,7 +55,7 @@ module LensExpansion =
             let sProp =
                 sprintf "
             static member %s =
-                compose
+                Compose
                     %s.%sStaticLens
                     %s.%s_StaticLens"
                     sLens

ebd40ae39b91d9c4ec4b783c755d15cb5691672d
Anthony Perez <anthony@amazingant.com>
2016-12-28 13:38:23 +0000

    Add `CheckExpansion` function under the FromXml test script
    This will be part of the build script at some point, but I need a way to
    quickly test the intermediate changes.

--- tests/FromXml/test.fsx
+++ tests/FromXml/test.fsx
@@ -19,3 +19,12 @@ type LoadSourceTypes =
         OutputPath="actual_expansion.fsx",
         OutputMode=Amazingant.FSharp.TypeExpansion.Provider.OutputMode.CreateSourceFile
         >
+
+
+let CheckExpansion() =
+    let f x =
+        System.IO.Path.Combine(__SOURCE_DIRECTORY__, x)
+        |> System.IO.File.ReadAllLines
+    let actual = f "actual_expansion.fsx"
+    let expected = f "expected_expansion.fsx"
+    actual = expected

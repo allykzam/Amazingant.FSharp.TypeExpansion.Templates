c4667e7bd4753abcbf60ca2e1f55e210cf64455d
Anthony Perez <anthony@amazingant.com>
2017-01-03 14:41:15 +0000

    Add RunTests build target to build script
    Runs the `test.fsx` script for each template, and runs all scripts under
    the `bad_tests` directory for each template's tests (none exist right
    now).

--- build.fsx
+++ build.fsx
@@ -28,6 +28,42 @@ Target "Package" (fun _ ->
         })
 )
 
+let runTestScript script =
+    let dir = Path.GetDirectoryName script
+    let template = Path.GetFileName dir
+    printfn "Running tests for the %s template" template
+    let (ranOkay, output) = Fake.FSIHelper.executeFSI dir script []
+    output |> Seq.iter (fun x -> System.Console.WriteLine x.Message)
+    let errors = output |> Seq.exists (fun x -> x.IsError)
+    if not ranOkay then
+        failwithf "FSI returned an error code after running the %s tests." template
+    if errors then
+        failwithf "FSI generated error messages while running the %s tests." template
+    printfn "Successfully ran tests for the %s template" template
+
+let runBadScript script =
+    let f1 = Path.GetDirectoryName >> Path.GetFileName
+    let f2 = Path.GetDirectoryName >> f1
+    let relPath = (f2 script) @@ (f1 script) @@ (Path.GetFileName script)
+    let relPath = relPath.Replace("\\", "/")
+    printfn "Running the test script '%s', which should result in errors" relPath
+    let dir = Path.GetDirectoryName script
+    let template = f2 script
+    let (ranOkay, output) = Fake.FSIHelper.executeFSI dir script []
+    output |> Seq.iter (fun x -> System.Console.WriteLine x.Message)
+    let errors = output |> Seq.exists (fun x -> x.IsError)
+    if ranOkay && not errors then
+        failwithf "FSI successfully ran a %s test that should have failed." template
+    printfn "Test script '%s' finished running with errors, as expected.\n" relPath
+
+Target "RunTests" (fun _ ->
+    !! "tests/**/test.fsx"
+    |> Seq.iter runTestScript
+
+    !! "tests/**/bad_tests/**.fsx"
+    |> Seq.iter runBadScript
+)
+
 Target "GitTag" (fun _ ->
     let tag = sprintf "%s" releaseNotes.NugetVersion
     Branches.tag "" tag
